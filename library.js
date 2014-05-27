3edc230a4b35c76e4878a9ba1f9a81b8169a8701
KenOKABE <ken@KenOKABEs-MacBook-Air.local>
2014-05-28 07:48:36 +0900

    commit

---
+++ library.js
@@ -0,0 +1,66 @@
+var winston = module.parent.require('winston'),
+    Meta = module.parent.require('./meta'),
+
+    Emailer = {},
+    ses;
+
+Emailer.init = function (app, middleware, controllers)
+{
+  console.log('===========emailer-amazonses init============');
+  var AWS = require('aws-sdk');
+  AWS.config.loadFromPath('./config.json');
+  ses = new AWS.SES({
+      apiVersion: '2010-12-01'
+  });
+
+  /*
+    var render = function (req, res, next)
+    {
+        res.render('admin/plugins/emailer-amazonses', {});
+    };
+
+    app.get('/admin/plugins/emailer-amazonses', middleware.admin.buildHeader, render);
+    app.get('/api/admin/plugins/emailer-amazonses', render);
+
+    */
+};
+
+Emailer.send = function (data) {
+
+    ses.sendEmail({
+            Source: data.from,
+            Destination: {
+                ToAddresses: data.to
+            },
+            Message: {
+                Subject: {
+                    Data: data.subject
+                },
+                Body: {
+                    Text: {
+                        Data: data.plaintext,
+                    }
+                }
+            }
+        },
+        function (err, data) {
+            if (err) throw err;
+            console.log('Email sent:');
+            console.log(data);
+        });
+
+};
+
+Emailer.admin = {
+    menu: function (custom_header, callback) {
+        custom_header.plugins.push({
+            "route": '/plugins/emailer-amazonses',
+            "icon": 'fa-envelope-o',
+            "name": 'Emailer (AmazonSES)'
+        });
+
+        callback(null, custom_header);
+    }
+};
+
+module.exports = Emailer;
